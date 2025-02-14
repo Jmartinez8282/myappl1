e2b6d8d20ba4b0ac80c4c82aa3a10ca3d0d17c0f
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2025-02-14 14:13:57 -0800

    End of day 2 componets

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+
 import {
   Text,
   View,
@@ -7,43 +8,32 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   TouchableNativeFeedback,
- 
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
 } from "react-native";
-
+import {useDeviceOrientation} from '@react-native-community/hooks'
 
 export default function Index() {
+  const orientation = useDeviceOrientation()
+ 
 
+console.log('orientation is:', orientation)
 
+  console.log(Dimensions.get('screen'))
   return (
-    <View style={styles.container}>
-     
-    <Text> Edit app/index.tsx to this screen.</Text>
-    {/* <Image source={require('../assets/images/react-logo.png')}/> */}
-    <TouchableNativeFeedback
-      onPress={() => console.log("Image Pressed!!!")}
-    >
-    {/* <Image blurRadius={10} fadeDuration={6000} source={{
-     
-      width: 200,
-      height:300,
-      uri:"https://picsum.photos/200/300"
-    }}/> */}
-    <View style={{width:200, height:70, backgroundColor:'lightblue'}}></View>
-    </TouchableNativeFeedback>
-    </View>
-
-
+    <View style={{backgroundColor:'orange', width:"100%", height: orientation ? "30%": "100%"}}></View>
   );
 }
 
-const backgroundStyle = {backgroundColor:"orange"}
+// const backgroundStyle = { backgroundColor: "orange" };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  backgroundColor:"cornflowerblue",
-  justifyContent:'center',
-  alignItems:"center"
-   
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+
+//   },
+// });
